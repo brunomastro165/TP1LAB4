ef9b347265717f338f546134b6c90f771f2e82a0
SR0jo <santiagomza12@gmail.com>
2024-03-21 13:38:25 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
+import { useState, useEffect } from 'react';
 import Container from "./components/AdminPage/Container";
 import NoticiaPage from "./components/AdminPage/Noticias/NoticiaPage";
 import Selector from "./components/Selector/Selector";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import SubirImagen from "./components/UploadFileTest/SubirImagen";
-import AgregarEmpresa from "./components/AgregarEmpresa/AgregarEmpresa";
 import Empresa from "./components/EmpresaPage/Empresa";
-
 function App() {
   return (
     <Router>
-      <Selector />
-      <Routes>
-        <Route path="/administrador" element={<Container />} />
-        <Route path="/usuario" element={<Container />} />
-        <Route path="/AdminNoticias" element={<NoticiaPage />} />
-        <Route path="/subirImagen" element={<SubirImagen />} />
-        <Route path="/puto" element={<Empresa />} />
-      </Routes>
+      <AppContent />
     </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+  const [showSelector, setShowSelector] = useState(true);
+  const [showSelector2, setShowSelector2] = useState(true);
+
+  useEffect(() => {
+    setShowSelector2(!location.pathname.startsWith("/empresa"));
+  }, [location]);
+
+  useEffect(() => {
+    setShowSelector(location.pathname !== "/");
+  }, [location]);
+
+  return (
+    <>
+      {showSelector && showSelector2 && <Selector />}
+      <Routes>
+      <Route path="/" element={<Selector />} />
+        <Route path="administrador" element={<Container />} />
+        <Route path="usuario" element={<Container />} />
+        <Route path="AdminNoticias" element={<NoticiaPage />} />
+        <Route path="subirImagen" element={<SubirImagen />} />
+        <Route path="/empresa/:id" element={<Empresa />} />
+      </Routes>
+    </>
   );
 }
 

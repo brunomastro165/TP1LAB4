cd62803ab1526b386bd4bd0f7c36c0d0e6f31d12
Bruno Mastropietro <brunomastro165@gmail.com>
2024-04-04 15:51:46 -0300

    Bugfixing

--- src/components/AdminPage/Empresas/Card.jsx
+++ src/components/AdminPage/Empresas/Card.jsx
@@ -297,7 +297,7 @@ const Card = (props) => {
                     htmlFor="quienesSomos"
                     className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
-                    Empresa (Ej. Google)
+                    ¿Quiénes somos?
                   </label>
                 </div>
               </div>
@@ -368,8 +368,6 @@ const Card = (props) => {
           <div className="absolute inset-0 bg-black opacity-50"></div>
         </div>
       )}
-
-    
 
       {error && (
         <>
--- src/components/AdminPage/Empresas/Container.jsx
+++ src/components/AdminPage/Empresas/Container.jsx
@@ -207,6 +207,7 @@ const Container = () => {
                   value={form.nombreEmpresa}
                   onChange={handleChange}
                   maxLength={50}
+                  required
                 />
                 <label
                   htmlFor="nombreEmpresa"
@@ -360,7 +361,7 @@ const Container = () => {
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
                   <input
-                    type="text"
+                    type="textarea"
                     name="quienesSomos"
                     id="quienesSomos"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -374,7 +375,7 @@ const Container = () => {
                     htmlFor="quienesSomos"
                     className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
-                    Empresa (Ej. Google)
+                    ¿Quiénes somos?
                   </label>
                 </div>
               </div>
